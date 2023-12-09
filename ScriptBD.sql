e25d7d566c5933f400738d3536f512cbaa2f13c3
CRISTO MAGNO <127151280+CristoMagno@users.noreply.github.com>
2023-12-09 10:22:53 -0600

    Update ScriptBD.sql

--- ScriptBD.sql
+++ ScriptBD.sql
@@ -1,37 +1,51 @@
-create database `bateria`;
-create table usuario(
-    usuario varchar (30), 
+create database `Voltium`;
+use Voltium;
+CREATE TABLE usuario(
+    id integer(10),
+    usuario varchar(20), 
     contra varchar(20), 
-    tipo varchar(20)
+    tipo varchar(20),
+    PRIMARY KEY (id)
 );
 
-create table almacenado(
+CREATE TABLE almacenado(
     lote integer(10), 
     origen varchar(50),
     pesoTotal float(10),
-    fecha date
+    fecha date,
+    PRIMARY KEY (lote)
 );
 
-create table clasificacion(
-    lote integer(10),
+CREATE TABLE clasificacion(
+    idC integer(10),
+    loteC integer(10),
     cantNReutili float(10),
     cantReutili float(10),
-    total float(10),
-    foreign key (total) references almacenado (pesoTotal),
-    foreign key (lote) references almacenado (lote)
+    PRIMARY KEY (idC),
+    FOREIGN KEY (loteC) REFERENCES almacenado (lote)
 );
 
-
-create table extraccion(
+CREATE TABLE extraccion(
     numDesmontaje integer(10),
-    fechaExtraccion date,
+    fechaExtraccion DATE,
     lote integer(10),
     ctiAluminio float(10),
     ctiCobre float(10),
     ctiCobalto float(10),
     ctiNiquel float(10),
     ctiManganeso float(10),
-    ctiAluminio varchar(10),
-    primary key (numDesmontaje),
-    foreign key (lote) references almacenado (lote)
+    PRIMARY KEY (numDesmontaje),
+    FOREIGN KEY (lote) REFERENCES almacenado (lote)
 );
+
+
+
+create user 'Normal' Identified by 'prime';
+grant insert on Voltium.usuario to 'Normal';
+grant select on Voltium.usuario to 'Normal';
+grant update on Voltium.usuario to 'Normal';
+show grants for 'Normal';
+
+create user 'Super' Identified by 'dios';
+GRANT ALL PRIVILEGES ON Voltium.usuario TO 'Super';
+SHOW GRANTS FOR 'Super';
